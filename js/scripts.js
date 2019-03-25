391b37e0f3af32cfc373f6259ca2e2af81ce32a8
daviekish <davie.kamweru5612@gmail.com>
2019-03-25 11:57:09 +0300

    Add a reset button, changes on javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
-var a = parseFloat(prompt("Enter dimension of first side"));
-var b = parseFloat(prompt("Enter dimension of second side"));
-var c = parseFloat(prompt("Enter dimendion of third side"));
-var sides = function(a, b, c) {
-  return (a + b > c && b + c > a && a + c > b)
+function triangleTracker() {
+  var a = parseFloat(document.getElementById("a").value);
+  var b = parseFloat(document.getElementById("b").value);
+  var c = parseFloat(document.getElementById("c").value);
+
+
+  if(isNaN(a,b,c) === true || (a<=0 || b<=0 || c<=0)){
+    alert("NOTHING")
+  }
+   else if ( a === b && b === c) {
+    alert("It's an Equiateral triangle.")
+  }
+
+  else if ( a === b || b === c || a === c) {
+    alert("This forms an isoceles triangle")
+  }
+  else if ((a+b)<=c || (b+c)<=a || (a+c)<=b ){
+    alert("INVALID MEASURMENTS!.. NOT A TRIANGLE.")
+  }
+  else if ( a !== b !== c) {
+    alert("this is a scalene")
+  }
 }
-if (sides(a, b, c) && a === b && b === c) {
-  alert("Its an Equiateral triangle.")
-} else if (sides(a, b, c) && a === b || b === c || a === c) {
-  alert("this forms an isoceles triangle")
-} else if (sides(a, b, c) && a != b != c) {
-  alert("this is a scalene")
-} else {
-  alert("INVALID MEASURMENTS!.. NOT A TRIANGLE.")
+
+function reset(){
+  location.reload();
 }
